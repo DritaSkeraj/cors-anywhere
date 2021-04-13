4fd751b6f2d6c3dc65b2e4cbc04ce66c1e27beeb
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-04-13 13:25:01 +0200

    adds vercel frontend url

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8080 || 3000;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = [parseEnvList(process.env.CORSANYWHERE_WHITELIST), "http://localhost:3000"];
+var originWhitelist = [parseEnvList(process.env.CORSANYWHERE_WHITELIST), "http://localhost:3000", "https://all-playlists-fe.vercel.app/"];
 function parseEnvList(env) {
   if (!env) {
     return [];
