ee595af3f9adbf1ce440f553fde8043495c53208
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-05-21 14:19:06 +0200

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
+var host = process.env.HOST || "0.0.0.0";
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080 || 3000;
 
@@ -8,42 +8,55 @@ var port = process.env.PORT || 8080 || 3000;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = [parseEnvList(process.env.CORSANYWHERE_WHITELIST), "http://localhost:3000", "https://all-playlists-fe.herokuapp.com", "https://all-playlists-fe.herokuapp.com/",  "https://all-playlists-fe.vercel.app", "http://localhost:5000", "https://allplaylists.herokuapp.com", "https://job-board-opal.vercel.app", "http://localhost:8080/"];
+var originWhitelist = [
+  parseEnvList(process.env.CORSANYWHERE_WHITELIST),
+  "http://localhost:3000",
+  "https://all-playlists-fe.herokuapp.com",
+  "https://all-playlists-fe.herokuapp.com/",
+  "https://all-playlists-fe.vercel.app",
+  "http://localhost:5000",
+  "https://allplaylists.herokuapp.com",
+  "https://job-board-opal.vercel.app",
+  "http://localhost:8080/",
+  "http://localhost:8080",
+];
 function parseEnvList(env) {
   if (!env) {
     return [];
   }
-  return env.split(',');
+  return env.split(",");
 }
 
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
-var checkRateLimit = require('./lib/rate-limit')(1000000000000)//(process.env.CORSANYWHERE_RATELIMIT);
+var checkRateLimit = require("./lib/rate-limit")(1000000000000); //(process.env.CORSANYWHERE_RATELIMIT);
 
-var cors_proxy = require('./lib/cors-anywhere');
-cors_proxy.createServer({
-  originBlacklist: originBlacklist,
-  originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
-  checkRateLimit: checkRateLimit,
-  removeHeaders: [
-    'cookie',
-    'cookie2',
-    // Strip Heroku-specific headers
-    'x-request-start',
-    'x-request-id',
-    'via',
-    'connect-time',
-    'total-route-time',
-    // Other Heroku added debug headers
-    'x-forwarded-for',
-    'x-forwarded-proto',
-    'x-forwarded-port',
-  ],
-  redirectSameOrigin: true,
-  httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
-    xfwd: false,
-  },
-}).listen(port, host, function() {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+var cors_proxy = require("./lib/cors-anywhere");
+cors_proxy
+  .createServer({
+    originBlacklist: originBlacklist,
+    originWhitelist: originWhitelist,
+    requireHeader: ["origin", "x-requested-with"],
+    checkRateLimit: checkRateLimit,
+    removeHeaders: [
+      "cookie",
+      "cookie2",
+      // Strip Heroku-specific headers
+      "x-request-start",
+      "x-request-id",
+      "via",
+      "connect-time",
+      "total-route-time",
+      // Other Heroku added debug headers
+      "x-forwarded-for",
+      "x-forwarded-proto",
+      "x-forwarded-port",
+    ],
+    redirectSameOrigin: true,
+    httpProxyOptions: {
+      // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+      xfwd: false,
+    },
+  })
+  .listen(port, host, function () {
+    console.log("Running CORS Anywhere on " + host + ":" + port);
+  });
